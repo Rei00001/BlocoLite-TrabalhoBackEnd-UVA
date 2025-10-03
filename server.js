d0c7264ea8ce72d321e615bd1aba2a2374385b73
Rei00001 <rhenansantosandrade@gmail.com>
2025-10-03 19:26:10 -0300

    Aducionando no script

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
